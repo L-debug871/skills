2fcf5b9e5d76a797e146af09badde4542a09ba09
L-debug871 <elontesla09@gmail.com>
2025-01-20 15:18:39 +0200

    Add files via upload

---
+++ rabbit.py
@@ -0,0 +1,8 @@
+# Name: Lerato Moholo
+# Date: 02/08/2024
+# Purpose: By using only print statements, write a program called 'rabbit.py'
+
+print("(\\\\               (\\/)")
+print("( '')    (\_/)   (.. )   //)")
+print("O(\")(\") (\\\'.'/) (\")(\")O (\" )")
+print("        (\")_(\")        ()()o")
